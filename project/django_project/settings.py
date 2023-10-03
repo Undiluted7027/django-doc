aeacaa6a45f1cfd2e352092a47e50b4561467d2d
root <root@finch>
2023-10-02 23:43:06 -0700

    Learnt about templates and code testing

--- project/django_project/settings.py
+++ project/django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR /"pages/templates/pages/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
